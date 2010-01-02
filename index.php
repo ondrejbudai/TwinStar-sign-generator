d909c53f53ce269f5942eb801363b007bc386c36
unknown <Ondra@.(none)>
2010-01-02 16:20:39 +0100

    First commit!

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Generátor podpisu z dat stažených z armory.twinstar.cz .
+ *
+ * Testováno na PHP 5.2 s nainstalovanou GD knihovnou
+ *
+ * Vytvořeno v NetBeans IDE.
+ *
+ */
+
+
+
+?>
